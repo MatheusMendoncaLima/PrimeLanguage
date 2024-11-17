1992d89c2f54291130567ee4df8caf9ee087cce6
Lukas Bariani <lukasbariani08@gmail.com>
2024-11-17 00:29:56 -0300

    dropdown niveis

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -4,3 +4,9 @@ const menu = document.querySelector('.menu')
 perfil.onclick = () => {
   menu.classList.toggle('ativo');
 }
+
+const hamburguer = document.querySelector('.hamburguer')
+const drop_nivel = document.querySelector('.drop-nivel')
+hamburguer.onclick = () => {
+  drop_nivel.classList.toggle('ativo');
+}
