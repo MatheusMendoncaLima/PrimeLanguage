40c3d58ce7a6bd81d9f9ba240da117c0903efd97
MatheusMendoncaLima <matheusmendima03@gmail.com>
2024-11-16 22:34:52 -0300

    preDropdown

--- scripts/trilhas.js
+++ scripts/trilhas.js
@@ -8,6 +8,8 @@ if(params.length>1){
 
 var dict = {};
 
+const allTagA = document.getElementsByTagName("a");
+
 params.forEach(param => {
     if(param.includes("=")){
     dict[param.split("=")[0]] =  param.split("=")[1]; 
@@ -17,5 +19,13 @@ params.forEach(param => {
 const language = dict["language"];
 const level = dict["level"];
 
+
+
+for(i=0; i< allTagA.length; i++){
+    allTagA[i].href+="?language="+language+"&level="+level;
+}
+
 const module = document.getElementById(level.toUpperCase());
+if(module!= null){
 module.style.display = "block";
+}
